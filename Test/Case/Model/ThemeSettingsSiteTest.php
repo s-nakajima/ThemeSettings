fd0735439612bb56dbb7b59fd8731eb8522674dc
宮川 貴子 <info@nekoget.net>
2014-05-20 16:22:35 +0900

    テストコード分割

--- Test/Case/Model/ThemeSettingsSiteTest.php
+++ Test/Case/Model/ThemeSettingsSiteTest.php
@@ -67,56 +67,92 @@ class ThemeSettingsSiteTest extends CakeTestCase {
 	}
 
 /**
- * testGetTheme()
+ * テーマ情報の取得 (1レコード分の配列）
  * @return void
  */
 	public function testGetTheme() {
 		//データがある場合
 		$ck = $this->ThemeSettingsSite->getTheme();
-		//var_dump($ck);
 		$this->assertTextEquals("UnitTestTheme", $ck['ThemeSettingsSiteValue']['value']);
-		//データを削除
-		$ck = $this->ThemeSettingsSite->delete(2);
-		$this->assertTrue($ck);
-		//データが無い状態
+	}
+
+/**
+ * testGetThemeNoData()該当のレコードが無い場合
+ * @return void
+ */
+	public function testGetThemeNoData() {
+		$ck = $this->ThemeSettingsSite->getTheme();
+		//データが無い状態をつくる。
+		if ($ck) {
+			$ck = $this->ThemeSettingsSite->delete($ck['ThemeSettingsSite']['id']);
+			$this->assertTrue($ck);
+		}
+		//データが無い状態の場合nullが戻る。
 		$ck = $this->ThemeSettingsSite->getTheme();
 		$this->assertNull($ck);
 	}
 
+/**
+ * テーマのアップデート
+ * @return void
+ */
 	public function testUpdateTheme() {
+		//stringの場合のupdate処理実行
 		$theme = 'Default';
 		$ck = $this->ThemeSettingsSite->updateTheme($theme);
 		$this->assertTrue($ck);
 		//名前を確認する
 		$ck = $this->ThemeSettingsSite->getThemeName();
 		$this->assertTextEquals($theme, $ck);
-		//配列の場合
+	}
+
+/**
+ * テーマのアップデート配列で登録
+ * @return void
+ */
+	public function testUpdateThemeArray() {
+		//配列の場合のupdate処理実行
+		$theme2 = array();
 		$theme2['ThemeSettingsSiteValue']['value'] = "test2";
 		$ck = $this->ThemeSettingsSite->updateTheme($theme2);
 		$this->assertTrue($ck);
 		//名前を確認する
 		$ck = $this->ThemeSettingsSite->getThemeName();
 		$this->assertTextEquals("test2", $ck);
+	}
+/**
+ * テーマのアップデート配列で登録 + 中身がブランク
+ * @return void
+ */
+	public function testUpdateThemeArrayBlank() {
 		//valueがブランク
+		$oldName = $this->ThemeSettingsSite->getThemeName();
+		$theme2 = array();
 		$theme2['ThemeSettingsSiteValue']['value'] = "";
 		$ck = $this->ThemeSettingsSite->updateTheme($theme2);
-		//$this->assertTrue(!$ck);
+		$this->assertFalse($ck); //update失敗
+		//データは下記変わらないので旧のまま。
+		$this->assertTextEquals($this->ThemeSettingsSite->getThemeName(), $oldName);
 	}
 
+/**
+ * テーマの新規登録
+ */
 	public function testCreateTheme() {
 		//データが無い場合を作る
-		$ck = $this->ThemeSettingsSite->delete(2);
-		$this->assertTrue($ck);
-		//問い合わせた結果データは無いのでnullが戻る
-		$ck = $this->ThemeSettingsSite->getThemeName();
-		$this->assertEquals(null, $ck);
-		//テーマを設定する
+		$ck = $this->ThemeSettingsSite->getTheme();
+		//データが無い状態をつくる。
+		if ($ck) {
+			//削除処理
+			$ck = $this->ThemeSettingsSite->delete($ck['ThemeSettingsSite']['id']);
+			$this->assertTrue($ck);
+		}
+		//テーマを設定する //insert
 		$theme = 'Default';
 		$ck = $this->ThemeSettingsSite->updateTheme($theme);
 		$this->assertTrue($ck);
-		//名前を確認する
+		//登録されている名前を確認する
 		$ck = $this->ThemeSettingsSite->getThemeName();
 		$this->assertTextEquals($theme, $ck);
-		//var_dump($this->ThemeSite->find('all'));
 	}
 }
